22e3b4aeb42366c7042376977b786b5c27a8e344
GabrielMinecraft02 <gabrielmersesse09@gmail.com>
2025-11-08 17:54:16 +0100

    Fix Render deployment: use PORT env variable

--- app.py
+++ app.py
@@ -85,3 +85,7 @@ def check():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  # Render fournit le PORT via l'environnement
+    app.run(host='0.0.0.0', port=port, debug=True)
